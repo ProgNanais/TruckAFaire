ea1d8c6904485cd48fdca0fdd31d5da1e8b3d82f
ProgNanais <anais.ravel73@gmail.com>
2023-09-12 22:44:58 +0200

    - Ajout d'un champs description dans le formulaire, ajustement du formulaire avec le keyboard - Ajout de fausses données pour commencer la mise en page d'une FlatList

--- Components/Formulaire.tsx
+++ Components/Formulaire.tsx
@@ -1,51 +1,77 @@
 import React, {useState} from 'react';
 import SelectDropdown from 'react-native-select-dropdown';
-import { View, Text, ScrollView, StyleSheet, Button, Alert, TextInput } from 'react-native';
+import { View, Text, ScrollView, StyleSheet, Button, Alert, TextInput, Platform, Keyboard, KeyboardAvoidingView, TouchableWithoutFeedback} from 'react-native';
 
 const Formulaire = ({navigation}) => {
     const [text, setText] = useState('');
     const [price, setPrice] = useState('');
+    const [value, onChangeText] = React.useState('Tu veux en dire plus ?')
     const categorie = ["Voyage", "Visite", "Zoo", "Parc d'attraction", "Musée"]
     const priorite = ["Très fortement", "Fortement", "Moyennement", "Faiblement", "Très faiblement"]
 
 
     return (
-        <View>
+        <KeyboardAvoidingView
+            behavior={Platform.OS === 'ios'? 'padding' : 'position'}
+            keyboardVerticalOffset={Platform.OS === 'ios'? 100 : -140}
+            style={formulaireStyle.container}
+        >
             <Text style={formulaireStyle.title}>
                 Quel est ce TAF ?
             </Text>
-            <TextInput
-                style={formulaireStyle.inputTitle}
-                placeholder="Un petit nom sympa ?"
-                defaultValue={text}
-            />
-            <SelectDropdown
-                data={categorie}
-                defaultButtonText="Quelle sorte de super TAF ?"
-                buttonStyle={formulaireStyle.selected}
-                buttonTextStyle={formulaireStyle.textSelect}
-                rowTextStyle={formulaireStyle.rowSelect}
-                dropdownStyle={formulaireStyle.rowDroptown}
-            />
-            <SelectDropdown
-                data={priorite}
-                defaultButtonText="Ça urge ?"
-                buttonStyle={formulaireStyle.selectedPriorite}
-                buttonTextStyle={formulaireStyle.textSelect}
-                rowTextStyle={formulaireStyle.rowSelect}
-                dropdownStyle={formulaireStyle.rowDroptown}
-            />
-            <TextInput
-                 style={formulaireStyle.inputTitleTwo}
-                 placeholder="Une estimation du prix (€€) ?"
-                 defaultValue={price}
-                 inputMode="numeric"
-            />
-        </View>
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                <View style={formulaireStyle.contentView}>
+                    <TextInput
+                        style={formulaireStyle.inputTitle}
+                        placeholder="Tu veux faire quoi ?"
+                        defaultValue={text}
+                    />
+                    <SelectDropdown
+                        data={categorie}
+                        defaultButtonText="Quelle sorte de super TAF ?"
+                        buttonStyle={formulaireStyle.selected}
+                        buttonTextStyle={formulaireStyle.textSelect}
+                        rowTextStyle={formulaireStyle.rowSelect}
+                        dropdownStyle={formulaireStyle.rowDroptown}
+                    />
+                    <SelectDropdown
+                        data={priorite}
+                        defaultButtonText="Ça urge ?"
+                        buttonStyle={formulaireStyle.selectedPriorite}
+                        buttonTextStyle={formulaireStyle.textSelect}
+                        rowTextStyle={formulaireStyle.rowSelect}
+                        dropdownStyle={formulaireStyle.rowDroptown}
+                    />
+                    <TextInput
+                        style={formulaireStyle.inputTitleTwo}
+                        placeholder="Une estimation du prix (€€) ?"
+                        defaultValue={price}
+                        inputMode="numeric"
+                    />
+                    <TextInput
+                        multiline
+                        numberofLines={10}
+                        onChangeText={text => onChangeText(text)}
+                        value={value}
+                        style={formulaireStyle.description}
+                     />
+                    <View style={formulaireStyle.btn}>
+                        <Button
+                            title="Valider"
+                            color="#8b32fd"
+                            onPress={() => navigation.navigate('Home')}
+                        />
+                    </View>
+                 </View>
+            </TouchableWithoutFeedback>
+        </KeyboardAvoidingView>
     )
 }
 
 const formulaireStyle = StyleSheet.create({
+    container: {
+        flex:1,
+    },
     title: {
         color: '#8b32fd',
         textAlign: 'center',
@@ -53,6 +79,9 @@ const formulaireStyle = StyleSheet.create({
         marginBottom: 20,
         fontSize: 30,
         fontWeight: 'bold',
+    },
+    contentView: {
+        flex: 1,
     },
     inputTitle: {
         marginRight: 30,
@@ -66,11 +95,11 @@ const formulaireStyle = StyleSheet.create({
         textAlign: 'center',
     },
     selected: {
-        position: 'relative',
+        marginRight: 30,
+        marginLeft: 30,
+        marginTop: 10,
         borderStyle: 'solid',
         borderColor: 'black',
-        left: 30,
-        top: 10,
         borderRadius: 5,
         borderWidth: 1,
         width: '83.5%',
@@ -88,20 +117,20 @@ const formulaireStyle = StyleSheet.create({
         fontSize: 14,
     },
     selectedPriorite: {
-        position: 'relative',
+        marginRight: 30,
+        marginLeft: 30,
+        marginTop: 10,
         borderStyle: 'solid',
         borderColor: 'black',
-        left: 30,
-        top: 20,
         borderRadius: 5,
         borderWidth: 1,
         width: '83.5%',
         height: 50,
     },
     inputTitleTwo: {
-        position: 'relative',
-        left: 30,
-        top: 30,
+        marginRight: 30,
+        marginLeft: 30,
+        marginTop: 10,
         borderStyle: 'solid',
         borderColor: 'black',
         borderRadius: 5,
@@ -109,6 +138,26 @@ const formulaireStyle = StyleSheet.create({
         height: 50,
         textAlign: 'center',
         width: '83.5%',
+    },
+    description: {
+        marginRight: 30,
+        marginLeft: 30,
+        marginTop: 10,
+        borderStyle: 'solid',
+        borderColor: 'black',
+        borderRadius: 5,
+        borderWidth: 1,
+        minHeight: 50,
+        textAlign: 'center',
+        width: '83.5%',
+    },
+    btn: {
+        height: 70,
+        marginTop: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+        textAlign: 'center',
     },
 });
 
--- Components/ListeTrucs.tsx
+++ Components/ListeTrucs.tsx
@@ -1,13 +1,67 @@
 import React from 'react';
-import { View, Text, ScrollView, StyleSheet, Button, Alert } from 'react-native';
+import { View, Text, ScrollView, StyleSheet, FlatList, StatusBar, Button, Alert } from 'react-native';
+
+
+
+const Item = ({ title, category, priority, price, description }) => {
+    return (
+        <View>
+            <Text style={{color: 'white'}}>{title}</Text>
+            <Text style={{color: 'white'}}>{category}</Text>
+            <Text style={{color: 'white'}}>{priority}</Text>
+            <Text style={{color: 'white'}}>{price}</Text>
+        </View>
+    )
+}
+
+const TrucAFaire = [
+    {
+        id: 1,
+        title: 'Planetarium',
+        category: 'Visite',
+        priority: 'ça urge',
+        price: '25',
+        description: 'Go à Saint Etienne en amoureuse, regardez les étoiles'
+    },
+    {
+        id: 2,
+        title: 'Peaugres',
+        category: 'Visite',
+        priority: 'Faiblement',
+        price: '50',
+        description: 'Allé en Ardèche en famille, à peaugres (avec ta cousine et bilou ?)'
+    },
+    {
+        id: 3,
+        title: 'Aller en Italie',
+        category: 'Voyage',
+        priority: 'Faiblement',
+        price: '2000',
+        description: 'Visitez l\'Italie (Région à définir ensemble) '
+    }
+]
+
+const renderItem = ({ item }) => (
+    <Item
+        title={item.title}
+        category={item.category}
+        priority={item.priority}
+        price={item.price}
+    />
+);
 
 const ListeTrucs = () => {
     return (
-        <ScrollView>
+        <View>
             <View style={styleListe.listeVide}>
                 <Text style={styleListe.title}>Pas encore de truc à faire ?</Text>
             </View>
-        </ScrollView>
+            <FlatList
+            data={TrucAFaire}
+            renderItem={renderItem}
+            keyExtractor={item => item.id}
+            />
+        </View>
     )
 };
 
